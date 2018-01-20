c315a8ec2ba6f581feebbcd0e2442ad1c1616fac
hefengly <1047568365@qq.com>
2018-01-20 14:08:12 +0800

    add updata-password

--- src/components/user/user.vue
+++ src/components/user/user.vue
@@ -3,7 +3,8 @@
     <div>
       <el-button type="success" icon="el-icon-circle-plus" round @click="addVisible = true">增加</el-button>
       <el-button type="danger" icon="el-icon-delete" round @click="isDelete()">删除</el-button>
-      <el-button type="primary" icon="el-icon-edit" round @click="isupdata()">修改</el-button>      
+      <el-button type="primary" icon="el-icon-edit" round @click="isupdata()">修改</el-button>
+      <el-button type="warning" icon="el-icon-view" round @click="isUpdataPassword()">修改密码</el-button>      
     </div>
     <el-table
       ref="singleTable"
@@ -55,6 +56,9 @@
         <el-form-item label="电话" :label-width="formLabelWidth">
           <el-input v-model="form.phone" auto-complete="off"></el-input>
         </el-form-item>
+        <el-form-item label="密码" :label-width="formLabelWidth">
+          <el-input v-model="form.password" auto-complete="off"></el-input>
+        </el-form-item>
       </el-form>
       <div slot="footer" class="dialog-footer">
         <el-button @click="addVisible = false">取 消</el-button>
@@ -95,6 +99,19 @@
       <div slot="footer" class="dialog-footer">
         <el-button @click="updataVisible = false">取 消</el-button>
         <el-button type="primary" @click="sureUpdata()">修 改</el-button>
+      </div>
+    </el-dialog>
+
+    <!-- 修改密码弹窗 -->
+      <el-dialog :title="username" :visible.sync="passwordVisible">
+      <el-form :model="passWordform">
+        <el-form-item label="密码" :label-width="formLabelWidth">
+          <el-input v-model="passWordform.password" auto-complete="off"></el-input>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="passwordVisible = false">取 消</el-button>
+        <el-button type="primary" @click="UpdataPassword()">确 定</el-button>
       </div>
     </el-dialog>
   </div>
@@ -140,94 +157,130 @@
           name: '',
           work: '',
           phone: '',
-          account: ''
+          account: '',
+          password:''
+        },
+        passWordform: {
+          password: ''
         },
         currentRow: null,
         addVisible: false,
         formLabelWidth: '120px',
         deleteVisible: false,
         updataVisible:false,
+        passwordVisible:false,
         theDeleteUser: [],
         updataForm:{}
       }
     },
 
 
-  methods: {
-    //获取点击的表单数组元素
-    handleCurrentChange(val) {
-        this.currentRow = val ;
-        this.updataForm = (val === null) ? {}:val ;
-        console.log(this.currentRow);
-    },
-    //为数组对象添加删除指定元素的方法
-    removeByValue(arr, val) {
-      for(var i = 0; i<arr.length; i++) {
-        if(arr[i] == val) {
-          arr.splice(i, 1);
-          break;
+    methods: {
+      //获取点击的表单数组元素
+      handleCurrentChange(val) {
+          this.currentRow = val ;
+          this.updataForm = (val === null) ? {}:val ;
+          console.log(this.currentRow);
+      },
+      //为数组对象添加删除指定元素的方法
+      removeByValue(arr, val) {
+        for(var i = 0; i<arr.length; i++) {
+          if(arr[i] == val) {
+            arr.splice(i, 1);
+            break;
+          }
         }
+      },
+
+      //添加用户数据
+      addUser() {
+        console.log(this.form);
+
+
+        this.addVisible = false;
+      },
+
+      //是否删除用户
+      isDelete() {
+        if(this.currentRow == null) {
+          this.feedbackMessage("请先选择要操作的用户信息",'warning')
+        } else {
+          this.deleteVisible = true;    
+          Vue.set(this.theDeleteUser, 0, this.currentRow);
+        }
+      },
+
+      //确定删除用户
+      sureDelete()  {
+        this.deleteVisible = false;
+        //发请求
+
+        //请求结果
+        console.log("已成功删除");
+        this.removeByValue(this.tableData,this.currentRow);
+        this.currentRow = null;
+      },
+
+      //是否修改用户
+      isupdata() {
+        if(this.currentRow == null) {
+          this.feedbackMessage("请先选择要操作的用户信息",'warning')
+        } else {
+          this.updataVisible = true;
+        } 
+      },
+
+      //确定修改
+      sureUpdata() {
+        this.updataVisible = false;
+
+        //发请求
+
+        //请求结果
+
+      },
+
+      //显示操作反馈信息
+      feedbackMessage(message,type) {
+        this.$message({
+          showClose: true,
+          message: message,
+          type: type
+        });
+      },
+
+      //是否修改密码
+      isUpdataPassword () {
+        if(this.currentRow == null) {
+          this.feedbackMessage("请先选择要操作的用户信息",'warning')
+        } else {
+          this.passwordVisible = true;
+        } 
+      },
+
+      //确定修改密码
+      UpdataPassword () {
+        if(this.passWordform.password == '') {
+          this.feedbackMessage("密码不能为空",'error')
+        } else {
+            this.passwordVisible = false;
+
+            //发请求
+
+            //请求结果
+            this.feedbackMessage("修改密码成功",'success')
+        }
+
       }
     },
 
-    //添加用户数据
-    addUser() {
-      console.log(this.form);
-
-
-      this.addVisible = false;
-    },
-
-    //是否删除用户
-    isDelete() {
-      if(this.currentRow == null) {
-        this.feedbackMessage("请先选择要操作的用户信息",'warning')
-      } else {
-        this.deleteVisible = true;    
-        Vue.set(this.theDeleteUser, 0, this.currentRow);
+    computed: {
+      //修改密码弹出框中用户名字的获取和拼接
+      username: function () {
+        var name = (this.currentRow == null) ? '':this.currentRow.name;
+        return "修改" + name+ "的登录密码";
       }
-    },
-
-    //确定删除用户
-    sureDelete()  {
-      this.deleteVisible = false;
-      //发请求
-
-      //请求结果
-      console.log("已成功删除");
-      this.removeByValue(this.tableData,this.currentRow);
-      this.currentRow = null;
-    },
-
-    //是否修改用户
-    isupdata() {
-      console.log(this.updataForm)
-      if(this.currentRow == null) {
-        this.feedbackMessage("请先选择要操作的用户信息",'warning')
-      } else {
-        this.updataVisible = true;
-      } 
-    },
-
-    //确定修改
-    sureUpdata() {
-      this.updataVisible = false;
-
-      //发请求
-
-      //请求结果
-
-    },
-
-    //显示操作反馈信息
-    feedbackMessage(message,type) {
-      this.$message({
-        showClose: true,
-        message: message,
-        type: type
-      });
     }
-  }
   }
 </script>
 
